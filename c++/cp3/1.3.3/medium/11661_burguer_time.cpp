70a39c7b70209041b973ad091f8d1ac0faeefe52
root <root@localhost.localdomain>
2018-01-03 01:39:12 +0000

    keep trying

--- c++/cp3/1.3.3/medium/11661_burguer_time.cpp
+++ c++/cp3/1.3.3/medium/11661_burguer_time.cpp
@@ -3,56 +3,34 @@ using namespace std;
 int main(int argc, const char *argv[])
 {
     int L, mindist;    
-    int fs, fe;
+    int count;
     string places;
     while(cin >> L){
-        int size = L;
         cin >> places;
         int fromStart = 0;
         int fromEnd = L - 1;
-        bool foundR = false;
-        bool foundD = false;
-        fs = 0;
-        fe = 0;
+        count = 0;
         mindist = 9999999;
-        while(size--){
-            if(places[fromStart] == 'Z'){
-                mindist = 0;
-                break;
-            }
-            if (places[fromEnd] == 'Z') {
-                mindist = 0;
-                break;
-            }
+        while(fromStart++ != L){
             if(places[fromStart] == 'R'){
-                fs = 0;
-                foundR = true;
-            }
-            if(places[fromStart] == 'D' && foundR){
-                if(fs < mindist) {
-                    mindist = fs;
-                    fs = 0;
+                while(places[fromStart++] != 'D'){
+                    count++;
+                    if(places[fromStart] == 'R'){
+                        count = 0;
+                    }
+                    else {
+                        mindist = 0;
+                        break;
+                    }
+                }  
+
+                if(places[fromStart] == 'D') {
+                    if(count < mindist) mindist = count;
                 }
+                else break;
             }
-            
-            if(places[fromEnd] == 'D'){
-                fe = 0;
-                foundD = true;
-            }
-            if(places[fromEnd] == 'R' && foundD){
-                if(fe < mindist) {
-                    mindist = fe;
-                    fe = 0;
-                }
-            }
-            fs++;
-            fe++;
-            fromStart++;
-            fromEnd--;
-           
         }
-        if(L)
-            printf("min %d\n", mindist);
+        printf("min %d\n", mindist);
     } 
     return 0;
 }
