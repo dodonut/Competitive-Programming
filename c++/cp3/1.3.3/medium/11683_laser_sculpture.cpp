da8944d6bc2b2faa08c60a8a44dbb50a2309abf7
root <root@localhost.localdomain>
2018-01-12 08:56:16 -0500

    damnnnnnn nigga

--- c++/cp3/1.3.3/medium/11683_laser_sculpture.cpp
+++ c++/cp3/1.3.3/medium/11683_laser_sculpture.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
 int main(int argc, char const* argv[])
 {
   int A, C, c;
-  int height, lenght;
-  int n, nbef;
+  int counter, i;
+  int vale, peak, Greatest;
   while (cin >> A >> C, A) {
-    int X[C] = {0};
+    int X[C+1] = {0};
     c = 0;
-    height = A;
-    lenght = C;
-    while (lenght--) {
-      cin >> X[c++];
-    }
-
-    lenght = C;
-    n = 0;
-    int c = 1;
-    bool allLineIsLess = true;
-    while (height-- && n != nbef) {
-      nbef = n;
-      while (c < lenght) {
-         if (X[c] >= height) {
-           n++;
-           allLineIsLess = false;
-         } 
-         c++;
+    while (c < C) cin >> X[c++];
+    counter = 0;
+    peak = X[0];
+    vale = X[0];
+    Greatest = X[0];
+    //too slow
+    // for (int i = 1; i <= height; i++) {
+    //   laserON = false;
+    //   for (int j = 0; j < lenght; j++) {
+    //     if (X[j] < i) {
+    //       if (!laserON) {
+    //         counter++;
+    //         laserON = true;
+    //       }
+    //     }
+    //     else {
+    //       laserON = false;
+    //     }
+    //   }
+    // }
+    for (i = 1; i < C-1; i++) {
+      if (X[i-1] <= X[i]) {
+        peak = X[i];
+        if (X[i] > X[i+1]) {
+          counter += peak - vale;
+        }
       }
-      c = 0;
-      if(allLineIsLess) n++;
+      else {
+        vale = X[i];
+      }
+      Greatest = max(X[i], Greatest);
     }
-    cout << n << endl;
+    Greatest = max(X[i+1], Greatest);
+    counter += A - Greatest;
+    counter += X[i+1] < X[i] ? peak - X[i+1]: peak-vale;
+    cout << counter << endl;
   }
 }
