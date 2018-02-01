68127b2cdda87ad960443bd47a9dbf459deaab26
darkkklan <viniciusvims@gmail.com>
2018-02-01 09:16:56 -0500

    finished.

--- c++/cp3/1.3.3/AD HOC/555_bridge_hand.cpp
+++ c++/cp3/1.3.3/AD HOC/555_bridge_hand.cpp
@@ -45,12 +45,19 @@ int dealer(char d)
 
 int cardValue(char cv)
 {
+  if(cv == '2') return 2;
+  if(cv == '3') return 3;
+  if(cv == '4') return 4;
+  if(cv == '5') return 5;
+  if(cv == '6') return 6;
+  if(cv == '7') return 7;
+  if(cv == '8') return 8;
+  if(cv == '9') return 9;
   if(cv == 'T') return 10;
   if(cv == 'J') return 11;
   if(cv == 'Q') return 12;
   if(cv == 'K') return 13;
   if(cv == 'A') return 14;
-  return (int)cv;
 }
 
 void playersCard(vector<vector<string>> & vss,const string& line,  int& D)
@@ -83,7 +90,6 @@ vector<int> sortSuits(vector<string>& vs)
   vector<int> suitSize;
   char suit = suits.at(0);
   int suitIndex = findSuit(vs, suit);
-  int lastTempSize = 0;
   while(!suits.empty()){
     if(suitIndex != -1){
       temp.push_back(vs.at(suitIndex));
@@ -91,8 +97,7 @@ vector<int> sortSuits(vector<string>& vs)
       suitIndex = findSuit(vs, suit);
     }
     else{
-      suitSize.push_back(temp.size() - lastTempSize);
-      lastTempSize = temp.size();
+      suitSize.push_back(temp.size());
       suits.erase(suits.begin());
       if(!suits.empty()){
         suit = suits.at(0);
@@ -104,14 +109,14 @@ vector<int> sortSuits(vector<string>& vs)
   return suitSize;
 }
 
-void sortNumber(vector<string>& vs, int start, int end) 
+void sortNumber(vector<string>& vs, int start, int end)
 {
   string card1, card2;
   for (int i = start; i < end - 1; i++) {
-    for (int j = start+1; j < end; j++) {
+    for (int j = i+1; j < end; j++) {
       card1 = vs[i];
       card2 = vs[j];
-      if(cardValue(card2[1]) < cardValue(card1[1])) 
+      if(cardValue(vs[j][1]) < cardValue(vs[i][1]))
         std::swap(vs[i],vs[j]);
     }
   }
@@ -123,10 +128,10 @@ void sortAll(vector<vector<string>>& vss)
   vector<int> suitsSize;
   iter(i,0,vss.size(),1){
     suitsSize = sortSuits(vss[i]);
-    sortNumber(vss[i], suitsSize[0], suitsSize[1]);
-    sortNumber(vss[i], suitsSize[1]+1, suitsSize[2]);
-    sortNumber(vss[i], suitsSize[2]+1, suitsSize[3]);
-    sortNumber(vss[i], suitsSize[3]+1, suitsSize.size());
+    sortNumber(vss[i], 0, suitsSize[0]);
+    sortNumber(vss[i], suitsSize[0] , suitsSize[1]);
+    sortNumber(vss[i], suitsSize[1] , suitsSize[2]);
+    sortNumber(vss[i], suitsSize[2] , suitsSize[3]);
   }
 }
 
@@ -136,6 +141,41 @@ void displayAllHands(const vector<vector<string>>& vss)
   displayHand(vss[1], 'W');
   displayHand(vss[2], 'N');
   displayHand(vss[3], 'E');
+}
+
+//second solution
+void createMapping(map<string, int>& deck)
+{
+  string suits[] {"C", "D", "S", "H"};
+  string numbers[] {"2","3","4","5","6","7","8","9","T","J","Q","K","A"};
+
+  int i,j,counter = 0;
+  string card;
+  iter(i,0,4,1){
+    iter(j,0,13,1){
+      card += suits[i] + numbers[j];
+      deck[card] = counter++;
+      card = "";
+    }
+  }
+}
+
+vector<pair<string,int>> playersCard(map<string, int>& deck, const string& line, int& D)   
+{
+  int i;
+  string card = "";
+  vector<pair<string,int>> vss;
+  iter(i,0,line.size(),1) {
+    card += line[i];
+    int value;
+    value = deck[card];
+    if(card.size() == 2) {
+      vss[ D ].push_back(make_pair(card,value));
+      card = "";
+      D = (D+1) % 4;
+    }
+  }
+
 }
 
 int main()
@@ -154,6 +194,9 @@ int main()
     sortAll(vss);
 
     displayAllHands(vss);
+    int i;
+    iter(i,0,4,1) vss[i].clear();
+
   }
 
 }
