1cf8c6a536cd6bdac5eeb4e1f8262cd88f0bbb77
tooManySand <viniciusvims@gmail.com>
2018-03-02 11:00:54 -0300

    some bugs

---
+++ c++/cp3/1.3.3/AD HOC/chess/696_How_many_knights.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+int knights(int rows, int col)
+{
+  int Min = min(rows, col);
+  int Max = max(rows, col);
+  if (Min == 1)
+    return Max;
+  if (Min == 2)
+  {
+    int r = Max % 4;
+    int d = Max / 4;
+    switch (r)
+    {
+    case 1:
+      return d * 4 + 1;
+    case 2:
+      return d * 4 + 3;
+    case 3:
+      return d * 4 + 4;
+    case 0:
+      return d * 4;
+    }
+  }
+  return ceil((float)(rows * col) / 2);
+}
+
+int main()
+{
+  int rows, cols;
+  while (cin >> rows >> cols && (rows || cols))
+  {
+    printf("%d knights may be placed on a %d row %d column board.\n", knights(rows, cols), rows, cols);
+  }
+}
