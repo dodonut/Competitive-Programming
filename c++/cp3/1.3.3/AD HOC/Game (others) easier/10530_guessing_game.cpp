e67bb8417c650eee2a594e02915c85076d1ee9d6
Vinicius Moraes de Souza <viniciusvims@gmail.com>
2018-03-19 18:03:36 -0300

    bug

--- c++/cp3/1.3.3/AD HOC/Game (others) easier/10530_guessing_game.cpp
+++ c++/cp3/1.3.3/AD HOC/Game (others) easier/10530_guessing_game.cpp
@@ -2,18 +2,28 @@
 #include <cstring>
 int main()
 {
-  char fword[8], sword[8];
+  char fword[8], sword[8], cont = 0;
   int guess, high = 11, low = 0, righton = 0;
   while (scanf("%d", &guess) && guess)
   {
+    if (cont == 14)
+    {
+      int p = 0;
+    }
     scanf(" %s %s", &fword, &sword);
-    if ((strcmp(sword, "high") == 0) && high > guess)
-      high = guess;
-    else if ((strcmp(sword, "low") == 0) && low < guess)
-      low = guess;
+    if (strcmp(sword, "high") == 0)
+    {
+      if (guess < high)
+        high = guess;
+    }
+    else if (strcmp(sword, "low") == 0)
+    {
+      if (guess > low)
+        low = guess;
+    }
     else
     {
-      if (guess <= low || guess >= high)
+      if (guess >= high || guess <= low || low > high || high < low)
         printf("Stan is dishonest\n");
       else
         printf("Stan may be honest\n");
--- c++/cp3/1.3.3/AD HOC/Game (others) easier/12239_bingo.cpp
+++ c++/cp3/1.3.3/AD HOC/Game (others) easier/12239_bingo.cpp
@@ -4,34 +4,34 @@ int main()
 {
   int a, b;
   bool won;
-  while (scanf("%d %d", &a, &b), a)
+  int *num, *match;
+   while (scanf("%d %d", &a, &b) && a && b)
   {
     won = true;
-    int *num = new int[a + 1];
-    int *match = new int[a + 1];
+    num = new int[a + 5];
+    match = new int[a + 5];
+    for (int i = 0; i <= a; i++)
+      match[i] = 0;
+
     for (int i = 0; i < b; i++)
       scanf("%d", &num[i]);
-    for (int i = 0; i <= a; i++)
-      printf("%d\n", match[i]);
-    for (int i = 0; i < a; i++)
-    {
-      for (int j = i + 1; j < a; j++)
-      {
+
+    for (int i = 0; i < b; i++)
+      for (int j = i + 1; j < b; j++)
         match[abs(num[i] - num[j])] = 1;
-      }
-    }
+
     for (int i = 1; i <= a; i++)
-    {
       if (match[i] == 0)
         won = false;
-      int aux = match[i];
-      int p = 0;
-    }
+
     if (won)
       printf("Y\n");
     else
       printf("N\n");
+
     delete[] match;
     delete[] num;
   }
+  delete match;
+  delete num;
 }
