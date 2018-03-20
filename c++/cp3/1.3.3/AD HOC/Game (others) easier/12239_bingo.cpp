c57d4d40fc525ae99a87581c5115077d864f43dd
tooManySand <viniciusvims@gmail.com>
2018-03-20 10:58:56 -0300

    did not understand 114

--- c++/cp3/1.3.3/AD HOC/Game (others) easier/12239_bingo.cpp
+++ c++/cp3/1.3.3/AD HOC/Game (others) easier/12239_bingo.cpp
@@ -1,37 +1,72 @@
-#include <iostream>
+// #include <cstdio>
+// #include <cmath>
+// int main()
+// {
+//   int a, b;
+//   bool won;
+//   int *num, *match;
+//    while (scanf("%d %d", &a, &b) && a && b)
+//   {
+//     won = true;
+//     num = new int[b + 5];
+//     match = new int[a + 5];
+//     for (int i = 0; i <= a; i++)
+//       match[i] = 0;
+
+//     for (int i = 0; i < b; i++)
+//       scanf("%d", &num[i]);
+
+//     for (int i = 0; i < b; i++)
+//       for (int j = i + 1; j < b; j++)
+//         match[(int)std::abs(num[i] - num[j])] = 1;
+
+//     for (int i = 1; i <= a; i++)
+//       if (!match[i])
+//         won = false;
+
+//     if (won)
+//       printf("Y\n");
+//     else
+//       printf("N\n");
+
+//     delete[] match;
+//     delete[] num;
+//   }
+//   delete match;
+//   delete num;
+// }
+
+#include <cstdio>
+#include <vector>
 #include <cmath>
+
+using namespace std;
+
 int main()
 {
-  int a, b;
-  bool won;
-  int *num, *match;
-   while (scanf("%d %d", &a, &b) && a && b)
+  int N, B;
+
+  while (scanf("%d%d", &N, &B), N)
   {
-    won = true;
-    num = new int[a + 5];
-    match = new int[a + 5];
-    for (int i = 0; i <= a; i++)
-      match[i] = 0;
+    vector<int> balls(B);
+    vector<bool> able(N + 1, false);
 
-    for (int i = 0; i < b; i++)
-      scanf("%d", &num[i]);
+    for (int i = 0; i < B; ++i)
+    {
+      scanf("%d", &balls[i]);
+      for (int j = i - 1; j >= 0; --j)
+        able[abs(balls[i] - balls[j])] = true;
+    }
 
-    for (int i = 0; i < b; i++)
-      for (int j = i + 1; j < b; j++)
-        match[abs(num[i] - num[j])] = 1;
+    bool valid(true);
 
-    for (int i = 1; i <= a; i++)
-      if (match[i] == 0)
-        won = false;
+    for (int i = 1; i < N + 1; ++i)
+      if (!able[i])
+        valid = false;
 
-    if (won)
+    if (valid)
       printf("Y\n");
     else
       printf("N\n");
-
-    delete[] match;
-    delete[] num;
   }
-  delete match;
-  delete num;
 }
---
+++ c++/cp3/1.3.3/AD HOC/Game (others) harder/114_simulation_wizardry.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <vector>
+
+int main(){
+  int m, n, hw, p, tp,i;
+  scanf("%d %d %d %d", &m, &n, &hw, &p);
+  std::vector<std::vector<int>> hp(p);
+  int ball[4];
+  for (i = 0; i < p; i++){
+
+  }
+}
