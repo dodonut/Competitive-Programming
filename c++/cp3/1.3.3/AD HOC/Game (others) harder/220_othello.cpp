02d585bd38c0b97604822e10e2ce5629b374dfe1
Vinicius Moraes de Souza <viniciusvims@gmail.com>
2018-04-03 17:21:47 -0300

    era pra fazer mais alguma coisa que n lembro no movimento

--- c++/cp3/1.3.3/AD HOC/Game (others) harder/220_othello.cpp
+++ c++/cp3/1.3.3/AD HOC/Game (others) harder/220_othello.cpp
@@ -52,7 +52,55 @@ void getAllMoves(char playerPiece) {
   }
 }
 
-void makeMove()
+std::pair<int, int> loopTo(std::pair<int, int> from, std::pair<int, int> to) {
+  std::pair<int, int> dir;
+  int d = abs(from.first - to.first);
+  int d2 = abs(from.second - to.second);
+  if (d == d2) {
+    if (from.first < to.first)
+      dir.first = 1;
+    else
+      dir.first = -1;
+    if (from.second < to.second)
+      dir.second = 1;
+    else
+      dir.second = -1;
+  } else if (from.first == to.first) {
+    dir.first = 0;
+    dir.second = 1;
+  } else {
+    dir.first = 1;
+    dir.second = 0;
+  }
+  return dir;
+}
+
+void bracket(char pp, std::pair<int, int> from, std::pair<int, int> to) {
+  std::pair<int, int> dir = loopTo(from, to);
+  int i = dir.first, j = dir.second;
+  int row = from.first, col = from.second;
+  while (row != to.first && col != to.second) {
+    board[row][col] = pp;
+    row += i;
+    col += j;
+  }
+  board[row][col] = pp;
+}
+
+int makeMove(int x, int y, char pp) {
+  bool validMove = false;
+  for (auto &&i : list) {
+    if (i.first.first == x && i.first.second == y) {
+      bracket(pp, i.first, i.second);
+      validMove = true;
+    }
+  }
+  if (!validMove)
+    return 0;
+  return 1;
+}
+
+void displayBoard() {}
 
 int main() {
   int N, i, j, cont, player;
@@ -70,9 +118,9 @@ int main() {
 
     scanf(" %c", &ch);
     player = ch == 'B' ? 0 : 1;
-    while (scanf(" %c", &cmd) && cmd != 'Q') {
+    getAllMoves(playerPiece);
+    while (scanf(" %c", &cmd)) {
       cont = 0;
-      getAllMoves(playerPiece);
       playerPiece = player == 0 ? 'B' : 'W';
       if (cmd == 'L') {
         if (!list.empty()) {
@@ -87,10 +135,25 @@ int main() {
         } else
           printf("No legal move.");
         printf("\n");
-        list.clear();
-        setPairs.clear();
+      } else if (cmd == 'M') {
+        char r, c;
+        int ans;
+        scanf(" %c %c", &r, &c);
+        ans = makeMove(r - '0', c - '0', playerPiece);
+        if (!ans) { // movement not allowed
+          player = (player + 1) % 2;
+          playerPiece = player == 0 ? 'B' : 'W';
+          getAllMoves(playerPiece);
+          board[r - '0'][r - '0'] = playerPiece;
+          makeMove(r - '0', c - '0', playerPiece);
+        }
+      } else {
+        displayBoard();
+        break;
       }
     }
+    list.clear();
+    setPairs.clear();
   }
   printf("Quiting....");
 }
