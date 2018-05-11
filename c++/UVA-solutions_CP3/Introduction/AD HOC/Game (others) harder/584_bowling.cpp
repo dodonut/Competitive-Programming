b08f6a16aadc0be1eb315e4bb28cbcd7a6bfe8fe
root <root@localhost.localdomain>
2018-05-11 12:48:37 -0300

    ver o tempo melhor

--- c++/UVA-solutions_CP3/Introduction/AD HOC/Game (others) harder/584_bowling.cpp
+++ c++/UVA-solutions_CP3/Introduction/AD HOC/Game (others) harder/584_bowling.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <chrono>
 
 int main()
 {
+
+    //std::ios_base::sync_with_stdio(false);
+    //std::cin.tie(NULL);
+    auto val = std::chrono::high_resolution_clock::now();
+    auto i = [&val]() { return std::chrono::high_resolution_clock::now() - val; };
     char buff[32], c;
     std::string input;
-    int score;
+    int score, tempVal;
     int frame, play = 0;
 
     auto valueOf = [](char &c) {
@@ -24,31 +30,44 @@ int main()
         score = 0;
         frame = 0;
         play = 0;
+        tempVal = 0;
+        //std::getline(std::cin, input);
         scanf(" %[^\n]", buff);
         input = buff;
+
         if (input == "Game Over")
             break;
 
         input.erase(std::remove(input.begin(), input.end(), ' '), input.end());
-        printf("input a: %s\n", input.c_str());
 
         for (auto i = 0ul; i < input.length(); i++)
         {
             c = input[i];
+            tempVal += valueOf(c);
             play++;
-            score += valueOf(c);
-            if (c == '/' && frame < 10)
-                score += valueOf(input[i + 1]);
-            else if (c == 'X' && frame < 10)
-                score += valueOf(input[i + 1]) + valueOf(input[i + 2]);
-
-            if (play == 2 || c == '/' || c == 'X')
+            if (frame >= 10)
+                break;
+            if (play > 1)
             {
+                if (c == '/')
+                    score += 10 + valueOf(input[i + 1]);
+                else
+                    score += tempVal;
                 frame++;
                 play = 0;
+                tempVal = 0;
+            }
+            else if (c == 'X')
+            {
+                score += input[i + 2] == '/' ? 20 : 10 + valueOf(input[i + 1]) + valueOf(input[i + 2]);
+                frame++;
+                play = 0;
+                tempVal = 0;
             }
         }
         printf("%d\n", score);
+        //std::cout << score << '\n';
     }
+    std::cout << i().count();
     return 0;
 }
