922bf8870ad4a773dfec3e051a80731f494c8637
Dodonut <viniciusvims@gmail.com>
2018-09-16 17:33:26 -0300

    still have to test

--- UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/394_mapmaker.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/394_mapmaker.cpp
@@ -21,9 +21,7 @@ int main()
     Array Arr;
     while (N--)
     {
-        Arr.d_pairs.clear();
-        Arr.C.clear();
-        scanf(" %s %d %d %d", &Arr.name, &Arr.size, &Arr.D);
+        scanf(" %s %d %d %d", &(Arr.name), &(Arr.base), &(Arr.size), &(Arr.D));
         for (int i = 0; i < Arr.D; i++)
         {
             scanf("%d %d", &L, &U);
@@ -31,15 +29,39 @@ int main()
         }
         n[Arr.name] = Arr;
     }
+    int Cd, Ld, Ud, C0, tot;
     while (R--)
     {
+        n[name].C.clear();
+        I.clear();
+
         scanf(" %s", name);
         dimensions_of_name = n[name].D;
+        C0 = n[name].base;
+        n[name].C.push_back(n[name].size);
         for (int i = 0; i < dimensions_of_name; i++)
         {
             scanf("%d", &dim);
-            //todo ler todas as dimensoes e colocar no vetor i, depois realizar o calculo seguindo a formula
-            //de tras pra frente e armazenar em c
+            I.push_back(dim);
+
+            Cd = n[name].C[i];
+            Ld = n[name].d_pairs[dimensions_of_name - i - 1].first;
+            Ud = n[name].d_pairs[dimensions_of_name - i - 1].second;
+            C0 -= Cd * Ld;
+            if (i < dimensions_of_name - 1)
+                n[name].C.push_back(Cd * (Ud - Ld + 1));
         }
+
+        tot = C0;
+        for (int i = 0; i < dimensions_of_name; i++)
+        {
+            tot += n[name].C[dimensions_of_name - i - 1] * I[i];
+        }
+        printf("%s[%d", name, I[0]);
+        for (int i = 1; i < dimensions_of_name; i++)
+        {
+            printf(", %d", I[i]);
+        }
+        printf("] = %d\n", tot);
     }
 }
