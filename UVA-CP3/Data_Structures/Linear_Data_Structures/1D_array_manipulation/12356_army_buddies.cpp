976b64721a0ca165a51a6343477b7f0e9ee93165
tooManySand <viniciusvims@gmail.com>
2018-09-24 22:51:42 -0300

    bosta

--- UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/12356_army_buddies.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/12356_army_buddies.cpp
@@ -1,42 +1,36 @@
 #include <algorithm>
 #include <iostream>
+#include <cstdio>
 #include <numeric>
 #include <vector>
 
-std::vector<int> army;
-
-void dead(int from, int to)
-{
-
-    auto f = std::lower_bound(army.begin(), army.end(), from);
-    auto t = std::upper_bound(army.begin(), army.end(), to);
-
-    if (std::distance(army.begin(), f - 1) >= 0)
-        printf("%d ", *(f - 1));
-    else
-        printf("* ");
-
-    if (std::abs(std::distance(army.end(), t)) > 0)
-        printf("%d", *t);
-    else
-        printf("*");
-
-    army.erase(f, t);
-}
+std::vector<int> left(100001), right(100001);
 
 int main()
 {
-    int S, B, from, to;
+    int S, B, l, r;
     while (scanf("%d %d", &S, &B), S || B)
     {
-        army.clear();
-        army.resize(S);
-        std::iota(army.begin(), army.end(), 1);
+        std::iota(left.begin(), left.begin() + S + 1, 0);
+        std::iota(right.begin(), right.begin() + S + 1, 1);
+        right[S] = 0;
 
-        for (int i = 0; i < B; i++)
+        while (B--)
         {
-            scanf("%d %d", &from, &to);
-            dead(from, to);
+            scanf("%d %d", &l, &r);
+
+            if (left[l] == 0)
+                printf("* ");
+            else
+                printf("%d ", left[l]);
+            left[right[r]] = left[l];
+
+            if (right[r] == 0)
+                printf("*");
+            else
+                printf("%d", right[r]);
+            right[left[l]] = right[r];
+
             printf("\n");
         }
         printf("-\n");
