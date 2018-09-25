44a2bd4006c4e4762ff7b4c2c208fee0237ab493
Vinicius Moraes de Sousa <vmsousa@tjgo.gov>
2018-09-25 10:16:01 -0300

    ac

--- UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/12356_army_buddies.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/1D_array_manipulation/12356_army_buddies.cpp
@@ -1,36 +1,35 @@
-#include <algorithm>
 #include <iostream>
-#include <cstdio>
 #include <numeric>
 #include <vector>
+#include <algorithm>
 
-std::vector<int> left(100001), right(100001);
+std::vector<int> left(100005), right(100005);
 
 int main()
 {
     int S, B, l, r;
     while (scanf("%d %d", &S, &B), S || B)
     {
-        std::iota(left.begin(), left.begin() + S + 1, 0);
-        std::iota(right.begin(), right.begin() + S + 1, 1);
+        std::iota(left.begin(), left.begin() + S, 0);
+        std::iota(right.begin(), right.begin() + S, 1);
         right[S] = 0;
 
-        while (B--)
+        for (int i = 0; i < B; i++)
         {
             scanf("%d %d", &l, &r);
+
+            right[left[l]] = right[r];
+            left[right[r]] = left[l];
 
             if (left[l] == 0)
                 printf("* ");
             else
-                printf("%d ", left[l]);
-            left[right[r]] = left[l];
+                printf("%d ", left[right[r]]);
 
             if (right[r] == 0)
                 printf("*");
             else
-                printf("%d", right[r]);
-            right[left[l]] = right[r];
-
+                printf("%d", right[left[l]]);
             printf("\n");
         }
         printf("-\n");
