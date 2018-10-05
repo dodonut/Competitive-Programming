11313c9dacd04b92bbd18bec3e2f1a2c16878933
Vinicius Moraes de Souza <viniciusvims@gmail.com>
2018-10-05 14:28:28 -0300

    ac

--- UVA-CP3/Data_Structures/Linear_Data_Structures/BIt_manipulation/10264_the_most_potent_corner.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/BIt_manipulation/10264_the_most_potent_corner.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 
 #define FLIP(x, y) (x ^ (1 << y))
 
 int main()
 {
-    int arr[1 << 14 + 1] = {0}, N, sum1, sum2, s, stot, sums[1 << 14 + 1] = {0};
+    int arr[1 << 14 + 1] = {0}, N, s, stot, sums[1 << 14 + 1] = {0};
 
     while (scanf("%d", &N) != EOF)
     {
@@ -14,17 +14,13 @@ int main()
             scanf("%d", &arr[i]);
         }
 
-        sum1 = -1;
-        sum2 = -1;
         stot = 0;
         for (int i = 0; i < (1 << N); i++)
         {
             s = 0;
             for (int j = 0; j < N; j++)
-            {
-                int k = FLIP(i, j);
-                s += arr[k];
-            }
+                s += arr[FLIP(i, j)];
+
             sums[i] = s;
         }
         for (int i = 0; i < (1 << N); i++)
@@ -32,11 +28,8 @@ int main()
             for (int j = 0; j < N; j++)
             {
                 int k = FLIP(i, j);
-                sum2 = sum1;
-                sum1 = sums[k];
-                printf("sum2:%d sum1:%d stot:%d\n", sum2, sum1, stot);
-                if (sum1 + sum2 > stot)
-                    stot = sum1 + sum2;
+                if (sums[k] + sums[i] > stot)
+                    stot = sums[k] + sums[i];
             }
         }
         printf("%d\n", stot);
