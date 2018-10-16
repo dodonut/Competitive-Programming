1f336a73c5e87cfa18a2aa5f57231949236f9bad
vinicius Moraes de Souza <viniciusvims@gmail.com>
2018-10-16 10:03:24 -0300

    ac

--- UVA-CP3/Data_Structures/Linear_Data_Structures/BIt_manipulation/11173_grey_codes.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/BIt_manipulation/11173_grey_codes.cpp
@@ -1,31 +1,12 @@
-#include <iostream>
-
-#define FLIP(x, y) (x ^= (1 << y))
+#include <cstdio>
 
 int main()
 {
-    unsigned int N, n, k, t, x[1 << 31], count, pos, val, tmp_pos;
-    bool one, inc;
+    unsigned int N, n, k;
     scanf("%d", &N);
     while (N--)
     {
         scanf("%d %d", &n, &k);
-        t = 1;
-        ++k;
-        while ((1 << t) < k)
-            t++;
-        pos = (1 << t);
-        val = pos >> 1;
-        one = true;
-        inc = true;
-        tmp_pos = 1;
-        while (pos > k)
-        {
-            if (one)
-                FLIP(val, 0);
-            else
-                FLIP(val, count);
-            for (int i = 0; i < tmp_pos; i++)
-        }
+        printf("%d\n", k ^ (k >> 1));
     }
 }
