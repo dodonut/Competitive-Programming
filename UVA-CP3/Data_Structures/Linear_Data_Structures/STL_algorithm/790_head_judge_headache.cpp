f1f42653331d8c4e2480045856b9d36f884d3873
tooManySand <viniciusvims@gmail.com>
2018-11-05 22:53:36 -0200

    dont know

--- UVA-CP3/Data_Structures/Linear_Data_Structures/STL_algorithm/790_head_judge_headache.cpp
+++ UVA-CP3/Data_Structures/Linear_Data_Structures/STL_algorithm/790_head_judge_headache.cpp
@@ -8,21 +8,22 @@ struct Problem
 {
     bool solved;
     int solved_time;
-    std::set<int> submissions;
+    std::multiset<std::pair<int, char>> submissions;
     Problem()
     {
         solved = false;
-        solved_time = 0;
+        solved_time = 100000;
         submissions.clear();
     }
 };
 
 struct Team
 {
-    Problem p[8];
+    Problem p[26];
     int n_prob_solved, total_time, team_number;
     Team()
     {
+        total_time = 0;
         n_prob_solved = 0;
         team_number = 27;
     }
@@ -66,26 +67,28 @@ int main()
                 max_contest = nt;
             if (s == 'Y')
             {
-                teams[nt].n_prob_solved++;
-                teams[nt].p[p - 'A'].solved_time = h * 60 + m;
+                if (!teams[nt].p[p - 'A'].solved)
+                    teams[nt].n_prob_solved++;
                 teams[nt].p[p - 'A'].solved = true;
+                if (teams[nt].p[p - 'A'].solved_time > h * 60 + m)
+                    teams[nt].p[p - 'A'].solved_time = h * 60 + m;
             }
-            teams[nt].p[p - 'A'].submissions.insert(h * 60 + m);
+            teams[nt].p[p - 'A'].submissions.insert(std::make_pair(h * 60 + m, s));
         }
 
         for (int i = 1; i <= max_contest; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < 26; j++)
             {
                 if (teams[i].p[j].solved)
                 {
                     for (auto &k : teams[i].p[j].submissions)
                     {
-                        if (teams[i].p[j].solved_time != k)
+                        if (k.second == 'N')
                             teams[i].total_time += 20;
                         else
                         {
-                            teams[i].total_time += k;
+                            teams[i].total_time += k.first;
                             break;
                         }
                     }
@@ -93,15 +96,20 @@ int main()
             }
         }
 
-        std::sort(teams, teams + max_contest, comp);
+        std::sort(teams + 1, teams + max_contest + 1, comp);
         int rank = 1;
-        for (int i = 1; i < max_contest; i++)
+        for (int i = 1; i <= max_contest; i++)
         {
-            printf("%4d%5d%5d%11d\n", rank, teams[i].team_number, teams[i].n_prob_solved, teams[i].total_time);
             if (i > 1 && !(teams[i].n_prob_solved == teams[i - 1].n_prob_solved && teams[i].total_time == teams[i - 1].total_time))
             {
                 rank = i;
             }
+            printf("%4d%5d", rank, teams[i].team_number);
+            if (teams[i].n_prob_solved != 0)
+                printf("%5d%11d", teams[i].n_prob_solved, teams[i].total_time);
+            puts("");
         }
+        if (n)
+            printf("\n");
     }
 }
