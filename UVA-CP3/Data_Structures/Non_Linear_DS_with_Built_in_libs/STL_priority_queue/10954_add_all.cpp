66e7ddc8c1072ca8cc03f3977626c1401e07afc9
dodonut <viniciusvims@gmail.com>
2018-12-17 21:37:02 -0200

    ac

---
+++ UVA-CP3/Data_Structures/Non_Linear_DS_with_Built_in_libs/STL_priority_queue/10954_add_all.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+
+int main(){
+    int t, val, tot_cost;
+    while(scanf("%d",&t),t){
+        std::priority_queue<int> q;
+        while(t--){
+            scanf("%d", &val);
+            q.push(val);
+        }
+        while(tot_cost <= )
+    }
+}
---
+++ UVA-CP3/Data_Structures/Non_Linear_DS_with_Built_in_libs/STL_priority_queue/1203_argus.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+struct Register
+{
+    int num, period, original;
+    Register(int n, int p) : num(n), period(p), original(p) {}
+    bool operator<(const Register &r) const
+    {
+        if (period == r.period)
+            return num > r.num;
+        return period > r.period;
+    }
+};
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+
+    char s[10];
+    int q_num, q_period, K;
+
+    std::priority_queue<Register> queue;
+    while (scanf("%s", s), s[0] != '#')
+    {
+        scanf("%d %d", &q_num, &q_period);
+        queue.push(Register(q_num, q_period));
+    }
+
+    scanf("%d", &K);
+    while (K--)
+    {
+        auto tmp = queue.top();
+        printf("%d\n", tmp.num);
+        queue.pop();
+        tmp.period += tmp.original;
+        queue.push(tmp);
+    }
+}
